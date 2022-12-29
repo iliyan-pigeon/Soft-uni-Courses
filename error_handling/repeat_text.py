4672c77af8027ed0a737d3599d67b459b1fb68f3
Iliyan Galabov <106007083+iliyan-pigeon@users.noreply.github.com>
2022-12-29 10:14:29 +0200

    Delete error_handling directory

--- error_handling/repeat_text.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        text = input("Please enter a text:")
-        amount = int(input("Please enter number:"))
-        result = text * amount
-        print(result)
-        break
-    except ValueError:
-        print("Oop! That was no valid number. Try again...")
--- error_handling/value_cannot_be_negative.py
+++
@@ -1,8 +0,0 @@
-class ValueCannotBeNegative(Exception):
-    """Value should be higher than zero"""
-
-
-for i in range(5):
-    number = int(input())
-    if number < 0:
-        raise ValueCannotBeNegative
